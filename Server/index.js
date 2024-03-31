948aef8fabbc1b3343d9b4ea948481b29f739da7
akashlitoriya <96015948+akashlitoriya@users.noreply.github.com>
2024-03-31 10:42:03 +0530

    Added DB configuration and Auth middleware

--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 
 //routes
 
+//connect to db
+const connectDB = require("./config/db");
+connectDB();
 //server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
